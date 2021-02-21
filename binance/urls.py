f2ce2a8dace8314954ef3e8c73bc4a0c3b9c862d
Tarique Anwer <itzmetariq@gmail.com>
2021-02-21 13:23:46 +0530

    class based view & button color

--- binance/urls.py
+++ binance/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from binance.views import KeyFormView
 
 app_name = 'binance'
 urlpatterns = [
-    path('keys/', views.keyform_view),
+    #path('keys/', views.keyform_view),
+    path('keys/', KeyFormView.as_view()),
     #path('login/', views.login),
     #path('index/', views.index)
 ]
--- binance/views.py
+++ binance/views.py
@@ -1,8 +1,45 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views import View
 from .forms import APIKeyForm
 from .models import APIKey
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
 # Create your views here.
+
+
+@method_decorator(login_required, name='dispatch')
+class KeyFormView(TemplateView):
+    form_class = APIKeyForm
+    template_name = 'keyform.html'
+    context = {}
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        self.context['form'] = form
+        return render(request, self.template_name, self.context)
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        try:
+            # try to fetch API keys
+            instance = APIKey.objects.get(user=user)
+            # create object of form
+            form = self.form_class(request.POST, instance=instance)
+            # check if form data is valid
+            if form.is_valid():
+                # save the form data to model
+                form.save()
+        except APIKey.DoesNotExist:
+            form = self.form_class(request.POST)
+            # check if form data is valid
+            if form.is_valid():
+                apikey = form.save(commit=False)
+                # commit=False tells Django that "Don't send this to database yet.
+                apikey.user = request.user  # Set the user object here
+                apikey.save()  # Now send it to DB
+        return render(request, self.template_name, {'form': form})
 
 
 @login_required
@@ -33,3 +70,5 @@ def keyform_view(request):
 
     context['form'] = form
     return render(request, "keyform.html", context)
+
+
